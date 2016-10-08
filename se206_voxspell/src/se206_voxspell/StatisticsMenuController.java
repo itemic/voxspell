d90bb66ab684a593498c8b71812138304bbdda22
Terran <terrankroft@gmail.com>
2016-10-08 17:06:09 +1300

    Add StatsMenuController

---
+++ se206_voxspell/src/se206_voxspell/StatisticsMenuController.java
@@ -0,0 +1,21 @@
+package se206_voxspell;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+public class StatisticsMenuController {
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private ComboBox<?> levelComboBox;
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+}
